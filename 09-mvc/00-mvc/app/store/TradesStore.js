bb94ac98233c2cf418b8c3f8b377e705f9c7b300
jlamadrid <juan.lamadrid@gmail.com>
2014-08-04 12:20:57 -0400

    replaced json rest store with an in memory store

--- 09-mvc/00-mvc/app/store/TradesStore.js
+++ 09-mvc/00-mvc/app/store/TradesStore.js
@@ -7,6 +7,69 @@ Ext.define('TL.store.TradesStore', {
         'Ext.data.reader.Json'
     ],
 
+    data: {
+        "success": "true",
+        "trades": [
+            {
+                "id": 1,
+                "date_open": "01/01/2015",
+                "date_closed": "02/01/2015",
+                "trade_number": 125,
+                "ticker": "IBM",
+                "long_short": "long",
+                "strategy": "iron condor",
+                "type": "close",
+                "option": "Buy to OPEN RUT SEPT +1080 / - 1145 / +1210 CALL's",
+                "entry": 13,
+                "current": 14,
+                "exit": 23,
+                "status": "open",
+                "return": .25,
+                "wl": "w",
+                "pl": 800.56,
+                notes: ""
+            },
+            {
+                "id": 2,
+                "date_open": "02/01/2015",
+                "date_closed": "02/01/2015",
+                "trade_number": 127,
+                "ticker": "IBM",
+                "long_short": "long",
+                "strategy": "calendar",
+                "type": "open",
+                "option": "Buy to OPEN RUT SEPT +1080 / - 1145 / +1210 CALL's",
+                "entry": 13,
+                "current": 14,
+                "exit": 23,
+                "status": "working",
+                "return": .35,
+                "wl": "w",
+                "pl": 400.56,
+                notes: ""
+            },
+            {
+                "id": 3,
+                "date_open": "04/01/2015",
+                "date_closed": "02/01/2015",
+                "trade_number": 127,
+                "ticker": "ORCL",
+                "long_short": "long",
+                "strategy": "bull put spread",
+                "type": "open",
+                "option": "Buy to OPEN RUT SEPT +1080 / - 1145 / +1210 CALL's",
+                "entry": 13,
+                "current": 14,
+                "exit": 23,
+                "status": "working",
+                "return": .75,
+                "wl": "w",
+                "pl": -300.56,
+                notes: ""
+            }
+        ]
+    },
+
     constructor: function(cfg) {
         var me = this;
         cfg = cfg || {};
@@ -21,11 +84,11 @@ Ext.define('TL.store.TradesStore', {
             model: 'TL.model.Trade',
             storeId: 'tradeStoreId',
 
-            groupField: 'type',
+            groupField: 'strategy',
 
             proxy: {
-                type: 'rest',
-                url : 'data/trades.json',
+                type: 'memory',
+                //url : 'data/trades.json',
 
                 reader: {
                     type: 'json',
@@ -71,7 +134,7 @@ Ext.define('TL.store.TradesStore', {
                         verb = name + 'd';
                     }
 
-                    Ext.Msg.alert(name, Ext.String.format("{0} user: {1}", verb, record.getId()));
+                    Ext.Msg.alert(name, Ext.String.format("{0} record: {1}", verb, record.getId()));
                 }
             }
         }, cfg)]);
